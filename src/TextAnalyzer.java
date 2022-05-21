9ad7af967728ea6833996626e85ada4c12aa9043
Stephen Sturges Jr <stephen.sturges.jr@outlook.com>
2022-05-21 14:14:06 -0400

    Comments added to explain portions of code.

--- src/TextAnalyzer.java
+++ src/TextAnalyzer.java
@@ -8,7 +8,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class TextAnalyzer{
     public static void main(String[] args) {
@@ -16,27 +15,31 @@ public class TextAnalyzer{
             URL url = new URL("https://www.gutenberg.org/files/1065/1065-h/1065-h.htm");
             BufferedReader input = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
 
+            // Define loop control variables and create ArrayList for analysis.
             Boolean readInput = true;
             Boolean printLines = false;
             Boolean emptyLine = false;
             ArrayList<String> textToAnalyze = new ArrayList<String>();
             String inputLine;
             while ((inputLine = input.readLine().replaceAll("<[^>]*>", "").replaceAll("’", "'")) != null && readInput) {
+                // Controls the start of the relevant text to analyze.
                 if (inputLine.equalsIgnoreCase("The Raven")) {
                     printLines = true;
                 }
+                // Removes empty lines from being included in the relevant text ArrayList.
                 if (inputLine.isEmpty()) {
                     emptyLine = true;
                 } else {
                     emptyLine = false;
                 }
+                // Controls the end of the relevant text to analyze.
                 if (inputLine.equalsIgnoreCase("*** END OF THE PROJECT GUTENBERG EBOOK THE RAVEN ***")) {
                     printLines = false;
                     readInput = false;
                 }
+                // Splits the String lines into string words and places them into the textToAnalyze array.
                 if (printLines == true && emptyLine == false) {
                     System.out.println(inputLine);
-                    // String outputTextArray[] = inputLine.split("&mdash|[^a-z[A-Z]]");
                     String outputTextArray[] = inputLine.split("&mdash|[^’'a-z[A-Z]]");
                     for (int i = 0; i < outputTextArray.length; i++) {
                         if (!outputTextArray[i].isEmpty()) {
@@ -45,17 +48,15 @@ public class TextAnalyzer{
                     }
                 }
             }
+            // Display Index of element and it's contents for debugging purposes.
             for (int i = 0; i < textToAnalyze.size(); i++) {
                 System.out.println("ArrayList Element: " + i + ", " + textToAnalyze.get(i));
             }
+            // Display a selected index's content for debugging purposes.
             int indexToDisplay = 69;
             System.out.println("Selected Array Element: " + indexToDisplay + ", Contents: " + textToAnalyze.get(indexToDisplay));
-            // String[] lines = textToAnalyze.get(indexToDisplay).split("&mdash|[^abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]");
-            // for (int i = 0; i < lines.length; i++) {
-            //     if (!lines[i].isEmpty()) {
-            //         System.out.println("Lines Array Element: " + i + ", " + lines[i]);
-            //     }
-            // }
+
+            // Count the number of occurances of a word in the ArrayList of text from the URL.
             ArrayList<String> results = new ArrayList<String>();
             for (int i = 0; i < textToAnalyze.size(); i++) {
                 int wordCount = 0;
@@ -69,6 +70,8 @@ public class TextAnalyzer{
                 results.add(word + ", " + wordCount);
                 wordCount = 0;
             }
+
+            // Display the results ArrayList.
             for (String string : results) {
                 System.out.println(string);
             }
