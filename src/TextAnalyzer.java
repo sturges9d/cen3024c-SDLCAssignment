af7330dc4759814bb987d1f2850a4c485c00b546
Stephen Sturges Jr <stephen.sturges.jr@outlook.com>
2022-05-21 14:03:41 -0400

    Program now counts the number of occurances of each word and outputs to an ArrayList. TODO: Sort the results.

--- src/TextAnalyzer.java
+++ src/TextAnalyzer.java
@@ -8,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class TextAnalyzer{
     public static void main(String[] args) {
@@ -47,7 +48,7 @@ public class TextAnalyzer{
             for (int i = 0; i < textToAnalyze.size(); i++) {
                 System.out.println("ArrayList Element: " + i + ", " + textToAnalyze.get(i));
             }
-            int indexToDisplay = 1071;
+            int indexToDisplay = 69;
             System.out.println("Selected Array Element: " + indexToDisplay + ", Contents: " + textToAnalyze.get(indexToDisplay));
             // String[] lines = textToAnalyze.get(indexToDisplay).split("&mdash|[^abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]");
             // for (int i = 0; i < lines.length; i++) {
@@ -55,6 +56,22 @@ public class TextAnalyzer{
             //         System.out.println("Lines Array Element: " + i + ", " + lines[i]);
             //     }
             // }
+            ArrayList<String> results = new ArrayList<String>();
+            for (int i = 0; i < textToAnalyze.size(); i++) {
+                int wordCount = 0;
+                String word = textToAnalyze.get(i);
+                for (int j = 0; j < textToAnalyze.size(); j++) {
+                    if (textToAnalyze.get(j).equalsIgnoreCase(word)) {
+                        wordCount++;
+                        textToAnalyze.remove(j);
+                    }
+                }
+                results.add(word + ", " + wordCount);
+                wordCount = 0;
+            }
+            for (String string : results) {
+                System.out.println(string);
+            }
         } catch (Exception e) {
             //TODO: handle exception
             e.printStackTrace();
