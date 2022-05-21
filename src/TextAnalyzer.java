81d672b470067cda932967b05bac80cb97a35c0d
Stephen Sturges Jr <stephen.sturges.jr@outlook.com>
2022-05-20 20:28:59 -0400

    Final array now outputs only the words on a given line.

--- src/TextAnalyzer.java
+++ src/TextAnalyzer.java
@@ -15,36 +15,38 @@ public class TextAnalyzer{
             URL url = new URL("https://www.gutenberg.org/files/1065/1065-h/1065-h.htm");
             BufferedReader input = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
 
-            Boolean printLines = false;
             Boolean readInput = true;
+            Boolean printLines = false;
+            Boolean emptyLine = false;
             String inputLine;
             ArrayList<String> textToAnalyze = new ArrayList<String>();
             while ((inputLine = input.readLine().replaceAll("\\<.*?>", "")) != null && readInput) {
                 if (inputLine.equalsIgnoreCase("The Raven")) {
-                    if (inputLine != "") {
-                        printLines = true;
-                    } else {
-                        printLines = false;
-                    }
+                    printLines = true;
+                }
+                if (inputLine.isEmpty()) {
+                    emptyLine = true;
+                } else {
+                    emptyLine = false;
                 }
                 if (inputLine.equalsIgnoreCase("*** END OF THE PROJECT GUTENBERG EBOOK THE RAVEN ***")) {
                     printLines = false;
                     readInput = false;
                 }
-                if (printLines == true) {
+                if (printLines == true && emptyLine == false) {
                     System.out.println(inputLine);
-                    if (inputLine.replaceAll("\\<.*?>", "") != "") {
-                        textToAnalyze.add(inputLine.trim());
-                    }
+                    textToAnalyze.add(inputLine.trim());
                 }
             }
             for (int i = 0; i < textToAnalyze.size(); i++) {
                 System.out.println("Array Contents: Element: " + i + ", " + textToAnalyze.get(i));
             }
-            System.out.println("Selected Array Contents: " + textToAnalyze.get(15));
-            String[] lines = textToAnalyze.get(15).split(" |&mdash|;");
+            System.out.println("Selected Array Contents: " + textToAnalyze.get(10)); // Change this index.
+            String[] lines = textToAnalyze.get(10).split("&mdash|[^abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]"); // Change this index.
             for (int i = 0; i < lines.length; i++) {
-                System.out.println("Lines Array Element: " + i + ", " + lines[i]);
+                if (!lines[i].isEmpty()) {
+                    System.out.println("Lines Array Element: " + i + ", " + lines[i]);
+                }
             }
         } catch (Exception e) {
             //TODO: handle exception
